071ace8d2888d236e387a38acf403db39c98f11c
GedariaArlene <85238537+GedariaArlene@users.noreply.github.com>
2021-06-26 05:21:05 +0800

    url

--- Eform/urls.py
+++ Eform/urls.py
@@ -1,27 +1,24 @@
 from django.contrib import admin
 from django.urls import path
 from . import views 
+from django.conf.urls import url 
 
 urlpatterns = [
 	path ('',views.EStudent),
 	path('students', views.List, name="students"),
 	path('requirement.html', views.Requirements, name="requirement"),
 	path('specialization.html', views.Specialization, name="specialization"),
+	path('modality.html', views.Modality, name="modality"),	
 	path('schedule.html', views.Schedule, name="schedule"),	
 	path('Done.html', views.Done, name="Done"),	
-	
+
+	url(r'^EditList/(?P<id>\d+)$', views.EditList, name="EditList"),
+	url(r'^EditList/UpdateList/(?P<id>\d+)$', views.UpdateList, name="UpdateList"),
+	url(r'^DeleteList/(?P<id>\d+)$', views.DeleteList, name="DeleteList"),
 
 	# path('edit/<int:id>', views.edit),  
  #    path('update/<int:id>', views.update),  
  #    path('delete/<int:id>', views.destroy),
 ]
 
-'''
-urlpatterns = [
-    path('', views.ActivityList.as_view(), name='activity_list'),
-    path('view/<int:pk>', views.ActivityDetail.as_view(), name='activity_detail'),
-    path('new', views.ActivityCreate.as_view(), name='activity_new'),
-    path('edit/<int:pk>', views.ActivityUpdate.as_view(), name='activity_edit'),
-    path('delete/<int:pk>', views.ActivityDelete.as_view(), name='activity_delete')
-]'''
 

485948654976c2b84cec8d5df33ffa3036ef4b1d
GedariaArlene <85238537+GedariaArlene@users.noreply.github.com>
2021-06-26 06:40:44 +0800

    adm

--- Eform/admin.py
+++ Eform/admin.py
@@ -10,12 +10,5 @@ admin.site.register(Modality)
 
 
 
-'''
-from django.contrib import admin
-from .models import Item, User
-#Register your models here.
-admin.site.register(Item)
-admin.site.register(User)'''
-
 
 

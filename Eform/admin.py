1d1ba4e55608952c6bbc5633b6413873124fa321
GedariaArlene <85238537+GedariaArlene@users.noreply.github.com>
2021-06-25 19:33:15 +0800

    bagoo

--- Eform/admin.py
+++ Eform/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from .models import Student, Requirements, ClassShift, Schedule, Specialization, StudentProfile
+from .models import Student, Requirements, Schedule, Specialization
 #Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Requirements)
-admin.site.register(ClassShift)
 admin.site.register(Schedule)
 admin.site.register(Specialization)
-admin.site.register(StudentProfile)
+
 
 
 '''
@@ -16,3 +15,6 @@ from .models import Item, User
 #Register your models here.
 admin.site.register(Item)
 admin.site.register(User)'''
+
+
+

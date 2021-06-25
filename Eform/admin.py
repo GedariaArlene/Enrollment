4247e3846618b5971e7bbd76c907e7ea91afadcb
GedariaArlene <85238537+GedariaArlene@users.noreply.github.com>
2021-06-26 05:21:49 +0800

    a

--- Eform/admin.py
+++ Eform/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Student, Requirements, Schedule, Specialization
+from .models import Student, Requirements, Schedule, Specialization, Modality
 #Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Requirements)
 admin.site.register(Schedule)
 admin.site.register(Specialization)
+admin.site.register(Modality)
 
 
 
